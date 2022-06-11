7b882ac878fefe24e306192eddc2af19a2c226f2
Filip Chalupa <chalupa.filip@gmail.com>
2022-06-11 20:39:02 +0200

    Client

---
+++ client/index.js
@@ -0,0 +1,69 @@
+import { colorRgbToHsl } from '../utils/colorRgbToHsl.js'
+import { delay } from '../utils/delay.js'
+
+const neewerServerIp = '192.168.0.251'
+
+const videoStream = await navigator.mediaDevices.getUserMedia({ video: true })
+
+const video = document.querySelector('video')
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+video.srcObject = videoStream
+
+const canvasSize = {
+	width: 100,
+	height: 50,
+}
+
+const lights = [
+	{
+		mac: 'C2:BF:C7:30:AA:F3',
+		pixel: {
+			x: Math.round(canvasSize.width / 4),
+			y: Math.round(canvasSize.height / 2),
+		},
+		element: document.querySelector('#color-1'),
+	},
+	{
+		mac: 'EC:C7:10:29:B1:22',
+		pixel: {
+			x: Math.round((canvasSize.width / 4) * 3),
+			y: Math.round(canvasSize.height / 2),
+		},
+		element: document.querySelector('#color-2'),
+	},
+]
+
+canvas.width = canvasSize.width
+canvas.height = canvasSize.height
+
+document.addEventListener('keyup', (event) => {
+	if (event.key === 'Escape') {
+		alert('Reload')
+		window.location.reload()
+	}
+})
+
+while (true) {
+	for (const light of lights) {
+		context.drawImage(video, 0, 0, canvasSize.width, canvasSize.height)
+		const [r, g, b] = context.getImageData(
+			light.pixel.x,
+			light.pixel.y,
+			1,
+			1,
+		).data
+		const hsl = colorRgbToHsl(r, g, b)
+
+		light.element.style.setProperty('--base', `rgb(${r}, ${g}, ${b})`)
+		light.element.style.setProperty('--hue', `hsl(${hsl.h}, 100%, ${hsl.l}%)`)
+
+		await fetch(
+			`http://${neewerServerIp}:8080/NeewerLite-Python/doAction?nopage&light=${
+				light.mac
+			}&mode=HSI&hue=${Math.floor(hsl.h)}&sat=${100}&brightness=${hsl.l}`,
+		)
+		await delay(500)
+	}
+}
--- utils/colorHexToHsl.js
+++ utils/colorHexToHsl.js
@@ -1,3 +1,5 @@
+import { colorRgbToHsl } from './colorRgbToHsl.js'
+
 // Based on https://gist.github.com/xenozauros/f6e185c8de2a04cdfecf
 export const colorHexToHsl = (hex) => {
 	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
@@ -7,34 +9,6 @@ export const colorHexToHsl = (hex) => {
 	let r = parseInt(result[1], 16)
 	let g = parseInt(result[2], 16)
 	let b = parseInt(result[3], 16)
-	;(r /= 255), (g /= 255), (b /= 255)
-	const max = Math.max(r, g, b),
-		min = Math.min(r, g, b)
-	const l = (max + min) / 2
-	let h = 0,
-		s = 0
-	if (max == min) {
-		h = s = 0 // achromatic
-	} else {
-		const d = max - min
-		s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
-		switch (max) {
-			case r:
-				h = (g - b) / d + (g < b ? 6 : 0)
-				break
-			case g:
-				h = (b - r) / d + 2
-				break
-			case b:
-				h = (r - g) / d + 4
-				break
-		}
-		h /= 6
-	}
 
-	return {
-		h: Math.round(h * 360),
-		s: Math.round(s * 100),
-		l: Math.round(l * 100),
-	}
+	return colorRgbToHsl(r, g, b)
 }
